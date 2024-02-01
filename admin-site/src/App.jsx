71c52eef8cce9bc17bc14169b2e47d5744d5dfdf
Varun-Karthik-T <varunkarthikceg@gmail.com>
2024-02-01 20:10:35 +0530

    shadcn setup done

---
+++ admin-site/src/App.jsx
@@ -0,0 +1,16 @@
+
+import { WorkshopStats } from "./pages/workshopstats.jsx"
+
+function App() {
+ 
+
+  return (
+    <>
+    <p>hi</p>
+    <WorkshopStats/>
+      
+    </>
+  )
+}
+
+export default App
---
+++ admin-site/src/constants/codes.jsx
@@ -0,0 +1,32 @@
+const codes = {
+    // workshops
+    GENERATIVEAI: { name: 'Generative AI Workshop', team: 1, cost: 799 },
+    ETHICALHACKING: { name: 'Cyber Security In The Age of AI Workshop', team: 1, cost: 999 },
+    IOTUNLEASED: { name: 'IOT Unleased Workshop', team: 1, cost: 599 },
+    BIMUSINGREVIT: { name: 'BIM using Revit Workshop', team: 1, cost: 349 },
+    BRAINWAVEROBO: { name: 'Brainwave Controlled Robotics Workshop', team: 1, cost: 1199 },
+    '3DANIMATION': { name: '3D Animation Workshop' },
+    FULLSTACKDEV: { name: 'Full Stack Development Workshop', team: 1, cost: 499 },
+    DATASCIENCE: { name: 'Data Science with Python Workshop', team: 1, cost: 499 },
+    SIXTHSENSEROBO: { name: 'Sixth Sense Robotics Workshop', team: 1, cost: 1199 },
+    DESIGNTHINKING: { name: 'Design Thinking For Space Colonization Workshop', team: 1, cost: 299 },
+    // events
+    GENERALREGISTRATION: { name: 'General Registration', team: 1, cost: 299 },
+    CIVILX: { name: 'Civil X', type: 'Iconic', team: 3, cost: 149 },
+    BYTEBASH: { name: 'Byte Bash', type: 'Iconic', team: 3, cost: 149 },
+    OSPC: { name: 'OSPC', type: 'Iconic', team: 2, cost: 199 },
+    PACBOT: { name: 'PacBot', type: 'Iconic', team: 4, cost: 499 },
+    ROWBOATICS: { name: 'Rowboatics', type: 'Iconic', team: 3, cost: 499 },
+    ROBORUSH: { name: 'RoboRush', type: 'Iconic', team: 4, cost: 499 },
+    PAPERPRESENTATION: { name: 'Paper Presentation', type: 'Iconic', team: 3, cost: 199 },
+    ENGINEERINGQUIZ: { name: 'Engineering Quiz', type: 'Iconic', team: 3, cost: 199 },
+    GODSPEED: { name: 'Godspeed', type: 'Flagship', team: 4, cost: 999 },
+    ROBOWARSFIFTEEN: { name: 'Robo Wars (15 KG)', type: 'Flagship', team: 5, cost: 999 },
+    ROBOWARSEIGHT: { name: 'Robo Wars (8 KG)', type: 'Flagship', team: 5, cost: 999 },
+    SKYSCULPTURE: { name: 'Sky Sculpture', type: 'Flagship', team: 6, cost: 999 },
+    DRONERACING: { name: 'Drone Racing', type: 'Flagship', team: 6, cost: 999 },
+    SHERLOCK: { name: 'Sherlockk & Wattson', type: 'Online', team: 1, cost: 0 },
+    RIDDLESPHINX: { name: 'Riddle of Sphinx', type: 'Online', team: 1, cost: 0 },
+ };
+ 
+ export default codes;
