0b05e62f7f4de10aa6e1a9bf0f11c77345529fc9
Salai-Kowshikan <salaikowshikan8743@gmail.com>
2024-02-10 09:27:45 +0530

    New structure

--- admin-site/src/pages/Events/index.jsx
+++ admin-site/src/pages/Events/index.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import DataTable from "@/components/DataTable/DataTable";
+import React, {useState} from "react";
 
 export default function EventsPage() {
+  const [data, setData] = useState(["summa", "summa2", "summa3"]);
   return (
     <div>
       <h1>Events</h1>
+      <DataTable headers={["Event", "Team", "Cost", "College", "Payment Status"]} data={data} />
     </div>
-  )
+  );
 }
--- admin-site/src/pages/MainPage/index.jsx
+++ admin-site/src/pages/MainPage/index.jsx
@@ -1,23 +1,14 @@
-import React from "react";
+import { useState } from "react";
 import { Outlet } from "react-router-dom";
-import Navbar  from "@/components/Navbar/Navbar";
-import DataTable from "@/components/DataTable/DataTable";
-import { headers } from "@/constants/codes";
+import Navbar from "@/components/Navbar/Navbar";
 
 export default function MainPage() {
-
-  let data = [
-    "summa",
-    "summa2",
-    "summa3",
-  ]
 
   return (
     <>
       <Navbar />
       <h1>Admin Page</h1>
       <Outlet></Outlet>
-      <DataTable headers={headers} data={data} />
     </>
   );
 }
